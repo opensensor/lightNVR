fa256cec9b418e1bd551ab4c5d66248f78dba23d
Matt Davis <matteius@gmail.com>
2026-03-05 13:19:13 -0500

    Check pt

--- web/js/version.js
+++
@@ -1,8 +0,0 @@
-/**
- * LightNVR version information
- * This file is auto-generated from CMakeLists.txt during the build process
- * DO NOT EDIT MANUALLY
- */
-
-export const VERSION = '0.28.3';
-export const GIT_COMMIT = '0a4a5221';
